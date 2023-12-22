afdd37c5957e56ec1f9880f7171fe59814e9e2d5
Richard Watterson <89240832+ins20@users.noreply.github.com>
2023-12-22 14:27:09 +0400

    Update useCalendar.ts

--- src/hooks/useCalendar.ts
+++ src/hooks/useCalendar.ts
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { nextMonth, prevMonth } from "@/utils/helpers";
-const useCalendar = (ransomDate: string, endDate: string) => {
+const useCalendar = () => {
   const [dateFirst, setDateFirst] = useState<any>(
-    new Date(new Date(endDate).getFullYear(), 11, 1)
+    new Date(2021, 11, 1)
   );
   const [dateSecond, setDateSecond] = useState<any>(
-    new Date(new Date(ransomDate).getFullYear(), 0, 1)
+    new Date(2022, 0, 1)
   );
   const handlePrevMonth = () => {
     setDateFirst(prevMonth(dateFirst));
